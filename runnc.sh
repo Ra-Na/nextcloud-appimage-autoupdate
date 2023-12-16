4b4ab971c5981908dd317dcdec6605407bd736da
Ra-Na <gluege@boerde.de>
2023-12-16 16:32:32 +0100

    Update runnc.sh

--- runnc.sh
+++ runnc.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sleep 5  # in case of autostart, wait some seconds for internet connection to come up
+# cd to Downloads folder (or wherever you keep the AppImages, change 
+# accordingly) in case the script is executed from somewhere else
+cd ~/Downloads
+
+sleep 10 # in case of autostart, wait some seconds for internet connection to come up
 
 # get latest release version 
 newest=$(curl 'https://github.com/nextcloud-releases/desktop/releases' | grep -oP '(?<=Link">Release )[.0-9]+' | head -1)
 
-echo -e "\n\n\nLatest version in github repo: $newest"
+echo -e "\n\n\n Latest version in github repo: $newest"
 
 # get latest version in folder
 current=$(ls Nextcloud-*AppImage | sort | tail -1 | grep -oP '(-)[.0-9]+' | cut -c2-)
-echo -e "Latest current verion in $(pwd): $current \n\n\n"
+echo -e " Latest current verion in $(pwd): $current \n\n\n"
 
 # download latest version if an update is available
 if [ "$current" != "$newest" ]; then
@@ -20,8 +24,10 @@ if [ "$current" != "$newest" ]; then
     eval "$cmd"
 fi
 
+
 # run latest version
 pre="./"
 string="$(ls Nextcloud-*AppImage | sort | tail -1)"
 post=" --background &!"
 eval $pre$string$post
+
