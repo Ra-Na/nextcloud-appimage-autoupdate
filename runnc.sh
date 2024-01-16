617b90f4b4a7e06177f6fdd98cd4b9cb5ef60b39
Ra-Na <gluege@boerde.de>
2024-01-16 21:39:10 +0100

    Update runnc.sh

--- runnc.sh
+++ runnc.sh
@@ -6,7 +6,6 @@ cd ~/Downloads
 
 sleep 10 # in case of autostart, wait some seconds for internet connection to come up
 
-
 wget -q --spider https://github.com
 
 if [ $? -eq 0 ]; then
@@ -18,21 +17,24 @@ echo "Latest version in github repo: $newest"
 
 # get latest version in folder
 current=$(ls Nextcloud-*AppImage | sort | tail -1 | grep -oP '(-)[.0-9]+' | cut -c2-)
-echo " Latest current verion in $(pwd): $current"
+echo "Latest current verion in $(pwd): $current"
 
 # download latest version if an update is available
 if [ "$current" != "$newest" ]; then
+    echo "Trying to update..."
     cmd="wget https://github.com/nextcloud-releases/desktop/releases/download/v$newest/Nextcloud-$newest-x86_64.AppImage"
     eval "$cmd"
     # make executable
     cmd="chmod +x Nextcloud-$newest-x86_64.AppImage"
     eval "$cmd"
 fi
+else
+echo "Offline."
 fi
 
 # run latest version
 pre="./"
 string="$(ls Nextcloud-*AppImage | sort | tail -1)"
 post=" --background &!"
+echo "Running $pre$string$post:" 
 eval $pre$string$post
-
