b26367fa28f7abfa275172d18e38309649fad810
rana is spanish for frog <gluege@boerde.de>
2024-07-18 23:17:28 +0200

    Update runnc.sh
    Due to constantly inconsistent info in the github repo, get the current version from nextcloud.com

--- runnc.sh
+++ runnc.sh
@@ -3,6 +3,7 @@
 # cd to Downloads folder (or wherever you keep the AppImages, change 
 # accordingly) in case the script is executed from somewhere else
 cd ~/Downloads
+rm latest
 
 sleep 10 # in case of autostart, wait some seconds for internet connection to come up
 
@@ -11,9 +12,9 @@ wget -q --spider https://github.com
 if [ $? -eq 0 ]; then
 echo "Online."
 # get latest release version 
-rm latest
-wget https://github.com/nextcloud-releases/desktop/releases/latest
-newest=$(grep "Release Release " latest | head -1 | grep -oP '[.0-9]+' )
+curl https://nextcloud.com/install/#install-clients > location
+downloadlink=$(grep unixOS location | grep -o -P '(?<=href=\").*(?=\" target)')
+newest=$(grep unixOS location | grep -o -P '(?<=href=\").*(?=\" target)' | grep -o -P '(?<=Nextcloud-).*(?=-x86)')
 
 echo "Latest version in github repo: $newest"
 
@@ -24,10 +25,11 @@ echo "Latest current verion in $(pwd): $current"
 # download latest version if an update is available
 if [ "$current" != "$newest" ]; then
     echo "Trying to update..."
-    cmd="wget https://github.com/nextcloud-releases/desktop/releases/download/v$newest/Nextcloud-$newest-x86_64.AppImage"
+    cmd="wget $downloadlink"
     eval "$cmd"
     # make executable
     cmd="chmod +x Nextcloud-$newest-x86_64.AppImage"
+    echo "$cmd"
     eval "$cmd"
 fi
 else
@@ -38,5 +40,5 @@ fi
 pre="./"
 string="$(ls Nextcloud-*AppImage | sort | tail -1)"
 post=" --background &!"
-echo "Running $pre$string$post:" 
+echo "Running $pre$string$post:"
 eval $pre$string$post
